ff290f6ea54585fdecb1132372c77b15e737d1a2
Tu Chu Quang <tucq88@gmail.com>
2014-01-22 16:58:18 +0700

    update

--- trunk/index.php
+++ trunk/index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Lorem ipsum dolor</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/font-awesome.css">
 <link rel="stylesheet" href="css/custom.css">
@@ -21,96 +22,146 @@
             <div class="clearfix"></div>
         </header>
         
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
+    </div>
+        
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
 
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img src="http://lorempixel.com/1280/400" alt="x">
-                    <div class="carousel-caption"></div>
-                </div>
-                <div class="item">
-                    <img src="http://lorempixel.com/1280/400/people" alt="y">
-                    <div class="carousel-caption"></div>
-                </div>
-                <div class="item">
-                    <img src="http://lorempixel.com/1280/400/business" alt="z">
-                    <div class="carousel-caption"></div>
-                </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <?php for ($i = 1; $i <= 5; ++$i):?>
+            <div class="item <?php if ($i==1) echo 'active'?>">
+                <img src="img/slides/istock-<?php echo $i?>.jpg" >
+                <div class="carousel-caption"></div>
             </div>
+            <?php endfor;?>
+        </div>
 
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"> 
-                <span class="glyphicon glyphicon-chevron-left"></span>
-            </a> 
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"> 
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
-        </div>
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"> 
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a> 
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"> 
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+    </div>
         
-        <br>
-        <div class="well well-lg">
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates aliquid atque doloribus nemo fuga qui accusantium velit maiores obcaecati laudantium voluptas deleniti id architecto assumenda culpa earum quae nesciunt facilis.</p>
-        </div>
-        
+    <div class="container">
         <!-- Features -->
-        <div class="row features">
-            <div class="col-lg-4">
-                <div class="block">
-                    <div class="text-center"><img src="http://lorempixel.com/150/150/technics" alt="" /></div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a libero quibusdam nemo sed doloremque nulla deserunt iusto. Autem dolorem debitis perspiciatis a harum animi totam quod delectus accusantium vel.</p>
-                </div>
+        <div class="content-block">
+            <div class="well well-md">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates aliquid atque doloribus nemo fuga qui accusantium velit maiores obcaecati laudantium voluptas deleniti id architecto assumenda culpa earum quae nesciunt facilis.</p>
             </div>
-            <div class="col-lg-4">
-                <div class="block">
-                    <div class="text-center"><img src="http://lorempixel.com/150/150/technics" alt="" /></div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a libero quibusdam nemo sed doloremque nulla deserunt iusto. Autem dolorem debitis perspiciatis a harum animi totam quod delectus accusantium vel.</p>            
+            <div class="row features">
+                <div class="col-md-4">
+                    <div class="block">
+                        <div class="text-center"><img data-src="holder.js/150x150" alt="" class="img-thumbnail"/></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a libero quibusdam nemo sed doloremque nulla deserunt iusto. Autem dolorem debitis perspiciatis a harum animi totam quod delectus accusantium vel.</p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="block">
-                    <div class="text-center"><img src="http://lorempixel.com/150/150/technics" alt="" /></div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a libero quibusdam nemo sed doloremque nulla deserunt iusto. Autem dolorem debitis perspiciatis a harum animi totam quod delectus accusantium vel.</p>
+                <div class="col-md-4">
+                    <div class="block">
+                        <div class="text-center"><img data-src="holder.js/150x150" alt="" class="img-thumbnail"/></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a libero quibusdam nemo sed doloremque nulla deserunt iusto. Autem dolorem debitis perspiciatis a harum animi totam quod delectus accusantium vel.</p>            
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="block">
+                        <div class="text-center"><img data-src="holder.js/150x150" alt="" class="img-thumbnail"/></div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate a libero quibusdam nemo sed doloremque nulla deserunt iusto. Autem dolorem debitis perspiciatis a harum animi totam quod delectus accusantium vel.</p>
+                    </div>
                 </div>
             </div>
         </div> <!-- ./ Features -->
-        
-        <!-- Product -->
-        <div>
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="text-center"><img src="http://lorempixel.com/250/250/technics" alt="" /></div>
-                </div>
-                <div class="col-lg-8">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
-                </div>
-                <div class="col-lg-4">
-                    <div class="text-center"><img src="http://lorempixel.com/250/250/technics" alt="" /></div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
-                </div>
-                <div class="col-lg-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
-                </div>
-            </div>
-        </div>
-        
-        
     </div>
+    
+    <div class="cover-1">
+        <div class="container">
+            <!-- Product -->
+            <div class="products content-block">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img data-src="holder.js/250x250" alt="" class="img-thumbnail"/>
+                    </div>
+                    <div class="col-md-8 caption">
+                        <h2 class="title">Lorem ipsum dolor</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-8 caption">
+                        <h2 class="title">Lorem ipsum dolor</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="text-center"><img data-src="holder.js/250x250" alt="" class="img-thumbnail"/></div>
+                    </div>
+                </div>
+                <div class="row third">
+                    <div class="col-md-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <img data-src="holder.js/300x350" class="img-thumbnail">
+                    </div>
+                    <div class="col-md-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste minus eligendi nobis mollitia reiciendis ipsam iure inventore deserunt similique? Maiores modi voluptatum error dicta distinctio expedita vel illo quisquam?</p>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- ./ Products -->
+        </div>
+    </div>
+    
+    
+    <div class="cover-2">
+        <div class="container">
+            <!-- Contact -->
+            <div class="contact">
+                <div class="intro">
+                    <h2 class="title">Lorem ipsum dolor ?</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Aut ex error esse aliquid debitis saepe impedit iure officia.</p>
+                </div>
+                <button class="btn btn-success">Contact</button>
+            </div><!-- ./Contact -->
+        </div>
+    </div>
+        
+    <div class="cover-3">
+        <div class="container">
+            <!-- Footer -->
+            <footer>
+                <div class="text-center">
+                    <ul class="list-inline links">
+                        <li><a href=""><img src="" alt="" data-src="holder.js/64x64/#a3e1a9:#82b487" class="img-thumbnail"/></a></li>
+                        <li><a href=""><img src="" alt="" data-src="holder.js/64x64/#a3e1a9:#82b487" class="img-thumbnail"/></a></li>
+                        <li><a href=""><img src="" alt="" data-src="holder.js/64x64/#a3e1a9:#82b487" class="img-thumbnail"/></a></li>
+                        <li><a href=""><img src="" alt="" data-src="holder.js/64x64/#a3e1a9:#82b487" class="img-thumbnail"/></a></li>
+                    </ul>
+                </div>
+            </footer><!-- ./Footer -->
+        </div>
+    </div>
+    
     <script src="js/jquery-1.9.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.placeholder.min.js"></script>
