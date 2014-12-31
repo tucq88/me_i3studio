7c9491ab871be822d592f52f899e82162aea0301
Tucq <tucq88@gmail.com>
2014-12-31 10:31:43 +0700

    WP | Add plugins + some settings

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -78,3 +78,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+/** TODO:Remove when deploy */
+define('FS_METHOD', 'direct');
